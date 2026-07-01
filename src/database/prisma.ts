6dfacaab773bc428ca38536cc7523bfc70fbde2c
AndrePassoni <andre.magalhaes.passoni@gmail.com>
2026-06-30 23:07:12 -0300

    feat: register teams and tasks routes, and export pg connection pool

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -4,7 +4,7 @@ import { PrismaPg } from '@prisma/adapter-pg'
 import { env } from '../env'
 
 const connectionString = env.DATABASE_URL
-const pool = new Pool({ connectionString })
+export const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 
 export const prisma = new PrismaClient({
